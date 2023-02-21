94633c6b4aafbe3656595fbd57e8851d3823ab59
sumanayanabadam <badamsumanayana@gmail.com>
2023-02-21 17:47:57 +0530

    Added toggle Button and other changes updated

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ submitBtn?.addEventListener('click', function () {
     let email = document.getElementById('email').value
     let address = document.getElementById('address').value
     let gender = document.getElementById('gender').value
-    // console.log("name",name)
   
+
     localStorage.setItem('is_name',name);
     localStorage.setItem('is_phnnum',phnnum);
     localStorage.setItem('is_age', age);
@@ -51,6 +51,32 @@ submitBtn?.addEventListener('click', function () {
     localStorage.setItem('is_email', email);
     localStorage.setItem('is_address', address);
     localStorage.setItem('is_gender', gender);
+
+    if(!name || !phnnum || !age || !country || !email || !address || !gender){
+      alert("Please Fill all the details..!")
+    }
+    
+    var row=1;
+    var display = document.getElementById('user_data');
+    var newRow = display.insertRow(row);
+    row++;
+
+    var cellName = newRow.insertCell(0);
+    cellName.innerHTML = name;
+    var cellPhnnum = newRow.insertCell(1);
+    cellPhnnum.innerHTML = phnnum;
+    var cellage = newRow.insertCell(2);
+    cellage.innerHTML = age;
+    var cellCountry = newRow.insertCell(3);
+    cellCountry.innerHTML = country;
+    var cellemail = newRow.insertCell(4);
+    cellemail.innerHTML = email;
+    var celladdress = newRow.insertCell(5);
+    celladdress.innerHTML = address;
+    var cellGender = newRow.insertCell(6);
+    cellGender.innerHTML = gender;
+
+    
 
     let username = localStorage.getItem('is_name')
     let contact = localStorage.getItem('is_phnnum')
@@ -61,14 +87,29 @@ submitBtn?.addEventListener('click', function () {
     let Gender = localStorage.getItem('is_gender')
 });
 
- 
+function toggleBtn(){
+  document.getElementById('user_data').classList.toggle("hidden")
+}
 
-function showBtn(){
-  document.getElementById('user_data').classList.remove("hidden");
+// function toggleBtn(){
+//   var show_hide = document.getElementById('user_data');
+//   if (show_hide.style.visibility === 'hidden'){
+//     show_hide.style.visibility = 'visible';
+//   }
+//   else{
+//     show_hide.style.visibility = 'hidden'
+//   }
+// }
+
+// function showBtn(){
+//   document.getElementById('user_data').classList.remove("hidden");
   
-}
-function hideBtn(){
-  document.getElementById('user_data').classList.add("hidden");
-}
+// }
+// function hideBtn(){
+//   document.getElementById('user_data').classList.add("hidden");
+// }
+
+
+
 
     
